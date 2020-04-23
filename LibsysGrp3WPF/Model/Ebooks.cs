8b029e2506ed894c135c289fba7725fc37d43593
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2020-04-23 14:07:28 +0200

    Skapade modeller

---
+++ LibsysGrp3WPF/Model/Ebooks.cs
@@ -0,0 +1,12 @@
+﻿namespace LibsysGrp3WPF.Model
+{
+    public class Ebooks
+    {
+        public int EbooksID { get; set; }
+        public int ItemsID { get; set; }
+        public int Pages { get; set; }
+        public string Size { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ LibsysGrp3WPF/Model/Visitors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LibsysGrp3WPF.Model
+{
+    public class Visitors
+    {
+        public int VisitorsID { get; set; }
+        public string IdentityNo { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime JoinDate { get; set; }
+        public string Password { get; set; }
+        public ushort Banned { get; set; }
+    }
+}
