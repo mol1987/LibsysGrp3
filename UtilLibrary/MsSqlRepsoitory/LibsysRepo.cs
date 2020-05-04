6ff27c5115985d01860f9298c1c2057c7ee7eb91
Firashanna <56591432+Firashanna@users.noreply.github.com>
2020-05-04 23:29:05 +0200

    ee

--- UtilLibrary/MsSqlRepsoitory/LibsysRepo.cs
+++ UtilLibrary/MsSqlRepsoitory/LibsysRepo.cs
@@ -24,7 +24,8 @@ namespace UtilLibrary.MsSqlRepsoitory
         #region constructor
         public LibsysRepo()
         {
-            _connectionString = ConfigurationManager.ConnectionStrings["LibsysAzure"].ConnectionString;
+            //_connectionString = ConfigurationManager.ConnectionStrings["LibsysAzure"].ConnectionString;
+            _connectionString = "Data Source=SQL6009.site4now.net;Initial Catalog=DB_A53DDD_mpol;User Id=DB_A53DDD_mpol_admin;Password=Password123;";
         }
         #endregion
 
