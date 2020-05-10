5e3422e442d44fa9a2c3f42ef1f7c760b7488546
AataMie <57095052+AataMie@users.noreply.github.com>
2020-05-10 16:55:47 +0200

    Update MainWindow startpage

--- LibsysGrp3WPF/ViewModel/MainViews/MainWindowViewModel.cs
+++ LibsysGrp3WPF/ViewModel/MainViews/MainWindowViewModel.cs
@@ -344,7 +344,7 @@ namespace LibsysGrp3WPF
             PageViewModels.Add(new VisitorSeminarViewModel());
 
 
-            CurrentPageViewModel = PageViewModels[8];
+            CurrentPageViewModel = PageViewModels[0];
 
             Mediator.Subscribe(PagesChoice.Page1, OnGoPage1Screen);
             Mediator.Subscribe(PagesChoice.Page2, OnGoPage2Screen);
