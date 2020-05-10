fb8c63d6aef5acdd062df4c1610887e24a2ecf57
AataMie <57095052+AataMie@users.noreply.github.com>
2020-05-10 16:45:37 +0200

    Added top search design for MainView
    And started to work with ManageBooksView.

--- LibsysGrp3WPF/ViewModel/MainViews/MainWindowViewModel.cs
+++ LibsysGrp3WPF/ViewModel/MainViews/MainWindowViewModel.cs
@@ -344,7 +344,7 @@ namespace LibsysGrp3WPF
             PageViewModels.Add(new VisitorSeminarViewModel());
 
 
-            CurrentPageViewModel = PageViewModels[0];
+            CurrentPageViewModel = PageViewModels[8];
 
             Mediator.Subscribe(PagesChoice.Page1, OnGoPage1Screen);
             Mediator.Subscribe(PagesChoice.Page2, OnGoPage2Screen);
