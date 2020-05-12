5fcf736bd7908b41c53b3a3870b1c1cbf3ea0ae2
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2020-05-12 20:13:05 +0200

    ListView added

--- LibsysGrp3WPF/ViewModel/Visitors/VisitorMyItemsViewModel.cs
+++ LibsysGrp3WPF/ViewModel/Visitors/VisitorMyItemsViewModel.cs
@@ -1,18 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
 using UtilLibrary.MsSqlRepsoitory;
 
 namespace LibsysGrp3WPF
 {
     public class VisitorMyItemsViewModel : BaseViewModel, IPageViewModel
     {
-        private void getItems()
+        private ObservableCollection<ItemsModel> _borrowed = new ObservableCollection<ItemsModel>
         {
-            // gets all borrowed items for visitor.
-            //var repo = new LibsysRepo();
-            //var tempBorrowList = repo.GetBorrowList<BorrowList>().Where(x => x.UsersCategory == (int)UsersCategory.Visitor);
-            //BorrowList = UsersModel.convertToObservableCollection(tempUsersList);
+            new ItemsModel()
+            {
+                ItemsID = 12,
+                ItemType = "book",
+                Title = "Sagan om Ringen",
+                Description = "Molles favorit",
+                Price = 99,
+                Date = new DateTime(2020-05-20)
+             },
+            new ItemsModel()
+            {
+                ItemsID = 8,
+                ItemType = "book",
+                Title = "XXX",
+                Description = "Jesses favorit",
+                Price = 99,
+                Date = new DateTime(2020-05-20)
+             }
+        };
+
+        public ObservableCollection<ItemsModel> borrowed
+        {
+            get
+            {
+                return _borrowed;
+            }
+            set
+            {
+                _borrowed = value;
+                OnPropertyChanged(nameof(borrowed));
+            }
         }
     }
 }
