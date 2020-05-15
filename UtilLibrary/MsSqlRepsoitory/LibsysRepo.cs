d406aeb82d065fa72dfff178a58c30608fbdd787
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2020-05-15 11:43:56 +0200

    fix 1.5

--- UtilLibrary/MsSqlRepsoitory/LibsysRepo.cs
+++ UtilLibrary/MsSqlRepsoitory/LibsysRepo.cs
@@ -1,13 +1,9 @@
 ﻿using Dapper;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UtilLibrary.MsSqlRepsoitory.Enums;
 
 namespace UtilLibrary.MsSqlRepsoitory
@@ -157,5 +153,5 @@ namespace UtilLibrary.MsSqlRepsoitory
         }
         #endregion
 
-            }
     }
+}
