82986d3f4a899d6692a124492b7fadf3416a78c4
AataMie <57095052+AataMie@users.noreply.github.com>
2020-05-16 20:54:57 +0200

    Added new display for startview
    Startview and Visitorsearchview has now list of all the books. Need to fix button for borrowing a book in Visitorsearchview.

--- LibsysGrp3WPF/ViewModel/Visitors/VisitorSearchViewModel.cs
+++ LibsysGrp3WPF/ViewModel/Visitors/VisitorSearchViewModel.cs
@@ -1,10 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace LibsysGrp3WPF
 {
-    public class VisitorSearchViewModel : BaseViewModel, IPageViewModel
+    public class VisitorSearchViewModel : ManageBookViewModel, IPageViewModel
     {
+        #region Privete properties
+        private string _btnborrowBook;
+        private ICommand _borrowBook;
+        #endregion
+
+        #region Public properties
+
+        public string BtnBorrowBook
+        {
+            get
+            {
+                return _btnborrowBook;
+            }
+
+            set
+            {
+                _btnborrowBook = value;
+                OnPropertyChanged(nameof(BtnBorrowBook));
+            }
+        }
+
+        public ICommand BorrowBook
+        {
+            get
+            {
+                return _borrowBook ?? (_borrowBook = new RelayCommand(x =>
+                {
+
+                    BtnBorrowBook = "Lånad";
+                }));
+            }
+        }
+
+        public VisitorSearchViewModel()
+        {
+            BtnBorrowBook = "Låna";
+        }
+
+
+        #endregion
     }
 }
