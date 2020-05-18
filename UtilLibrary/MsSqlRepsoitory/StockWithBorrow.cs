88ddfdb2013334d165bfb7c6fdb51c874ad23fa8
mol1987 <57093339+mol1987@users.noreply.github.com>
2020-05-18 22:40:27 +0200

    [start]

--- UtilLibrary/MsSqlRepsoitory/StockWithBorrow.cs
+++ UtilLibrary/MsSqlRepsoitory/StockWithBorrow.cs
@@ -1,10 +1,15 @@
-﻿namespace UtilLibrary.MsSqlRepsoitory
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilLibrary.MsSqlRepsoitory
 {
-    public class Stock : IStock
+    public class StockWithBorrow : IStock
     {
         public int StockID { get; set; }
         public int ItemsID { get; set; }
         public bool Available { get; set; }
         public string Reason { get; set; }
+
     }
 }
---
+++ UtilLibrary/MsSqlRepsoitory/StockWithBorrowInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilLibrary.MsSqlRepsoitory
+{
+    public class StockWithBorrowInfo : IStock
+    {
+    }
+}
