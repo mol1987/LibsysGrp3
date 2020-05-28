b19a28d06303ffcfde1d38fa9cfc06badf95aa1d
AataMie <57095052+AataMie@users.noreply.github.com>
2020-05-28 12:12:50 +0200

    update

--- LibsysGrp3WPF/ViewModel/MainViews/MainWindowViewModel.cs
+++ LibsysGrp3WPF/ViewModel/MainViews/MainWindowViewModel.cs
@@ -344,7 +344,7 @@ namespace LibsysGrp3WPF
             PageViewModels.Add(new VisitorSeminarViewModel());
             PageViewModels.Add(new ManageCheckInViewModel());
 
-            CurrentPageViewModel = PageViewModels[(int)PagesChoice.pageManageCheckIn];
+            CurrentPageViewModel = PageViewModels[(int)PagesChoice.pageManageUsers];
 
             Mediator.Subscribe(PagesChoice.pageStartView, OnGoStartPage);
             Mediator.Subscribe(PagesChoice.pageManageVisitor, OnGoPageManageVisitor);
