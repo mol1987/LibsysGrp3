c0f24799fb594c92ac414435130236b3a9d27773
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2020-05-30 11:18:18 +0200

    Complete

--- LibsysGrp3WPF/Model/Items/FullBorrowListModel.cs
+++ LibsysGrp3WPF/Model/Items/FullBorrowListModel.cs
@@ -14,7 +14,7 @@ namespace LibsysGrp3WPF.Model.Items
         public string Author { get; set; }
         public DateTime BorrowDate { get; set; }
         public DateTime DueDate { get; set; }
-        public DateTime ReturnDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
 
     }
 }
